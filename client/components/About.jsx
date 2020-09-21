7a7b7dd72214877ab9f57a9deda9df2d177fa82a
Jake-Hurley <jshurley97@gmail.com>
2020-09-21 13:35:56 +1200

    mobile styling complete

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -5,11 +5,12 @@ const About = () => {
         <>
             <h1 className='about-me' id='about'>About Me</h1>
             <div role='about container' className='about-container' >
+                <p className='nice-to-meet-you'>
+                Nice to meet you!
+                </p>
                 <p className='about-statement'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries.
+                I am a 23 year old Full Stack Developer living in Auckland New Zealand. With a background in Visual Effects and Digital Design, I love making creative technology with great user experience in mind.
+                When I am not at work, I use my free time to make fun side projects that channel my creativity side and improve my skills as a dev.
                 </p>
                 <div role='tech stack icons' className='stack-icons'>
                     <img src='/icons/javascript.svg' alt='javascript-icon' className='icon'/>
